070d46cb6cc17e99370206894c3ebf857ce909df
willyaditya <willyaditya03@gmail.com>
2020-10-06 18:52:27 +0700

    Update Brand.java

--- src/main/java/bca/Hapepedia/entity/Brand.java
+++ src/main/java/bca/Hapepedia/entity/Brand.java
@@ -1,7 +1,20 @@
 package bca.Hapepedia.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Entity
 @Table(name = "tbl_brand")
 public class Brand {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
 }
