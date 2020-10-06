3db83d7a56bbe76366629d9b60f571fa373909bc
willyaditya <willyaditya03@gmail.com>
2020-10-07 00:30:25 +0700

    Province entity, repo, service, and dto

---
+++ src/main/java/bca/Hapepedia/repo/ProvinceRepo.java
@@ -0,0 +1,9 @@
+package bca.Hapepedia.repo;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import bca.Hapepedia.entity.Province;
+
+public interface ProvinceRepo extends PagingAndSortingRepository<Province, Long>{
+
+}
