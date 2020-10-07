78b9f74d646d0fd983e7a476fa5b42f9bb485e16
annisaaadindra <annisaaadindra@gmail.com>
2020-10-07 11:07:08 +0700

    add entity varian

---
+++ src/main/java/bca/Hapepedia/entity/Varian.java
@@ -0,0 +1,43 @@
+package bca.Hapepedia.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_varian")
+public class Varian {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String name;
+
+    public Varian() {
+    }
+
+    public Varian(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
