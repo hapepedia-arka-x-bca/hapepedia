9b616d4669fe88ed2f2c2625fdd7b9a14caa2a75
willyaditya <willyaditya03@gmail.com>
2020-10-07 15:45:47 +0700

    Product entity, repo, service and dto

---
+++ src/main/java/bca/Hapepedia/dto/ProductForm.java
@@ -0,0 +1,71 @@
+package bca.Hapepedia.dto;
+
+import javax.validation.constraints.NotEmpty;
+
+public class ProductForm {
+	private Long id;
+
+	@NotEmpty(message = "Product Name is required.")
+	private String name;
+	
+	@NotEmpty(message = "Brand is required.")
+	private Long id_brand;
+	
+	@NotEmpty(message = "Category is required.")
+	private Long id_category;
+	
+	@NotEmpty(message = "Specification is required.")
+	private String specification;
+	
+	@NotEmpty(message = "Weight is required.")
+	private double weight;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId_brand() {
+		return id_brand;
+	}
+
+	public void setId_brand(Long id_brand) {
+		this.id_brand = id_brand;
+	}
+
+	public Long getId_category() {
+		return id_category;
+	}
+
+	public void setId_category(Long id_category) {
+		this.id_category = id_category;
+	}
+
+	public String getSpecification() {
+		return specification;
+	}
+
+	public void setSpecification(String specification) {
+		this.specification = specification;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+}
---
+++ src/main/java/bca/Hapepedia/entity/Product.java
@@ -0,0 +1,84 @@
+package bca.Hapepedia.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "brand_id", referencedColumnName = "id")
+	private Brand brand;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id", referencedColumnName = "id")
+	private Category category;
+	
+	@Column(length = 500, nullable = false)
+	private String specification;
+	
+	@Column(nullable = false)
+	private double weight;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Brand getBrand() {
+		return brand;
+	}
+
+	public void setBrand(Brand brand) {
+		this.brand = brand;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public String getSpecification() {
+		return specification;
+	}
+
+	public void setSpecification(String specification) {
+		this.specification = specification;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+}
