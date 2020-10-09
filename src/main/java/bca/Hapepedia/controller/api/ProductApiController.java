09dcf83557b40db17dc674f0d8c11e52dbf53633
annisaaadindra <annisaaadindra@gmail.com>
2020-10-09 09:37:37 +0700

    Update ProductApiController.java
    Add error handling

--- src/main/java/bca/Hapepedia/controller/api/ProductApiController.java
+++ src/main/java/bca/Hapepedia/controller/api/ProductApiController.java
@@ -1,11 +1,13 @@
 package bca.Hapepedia.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import bca.Hapepedia.entity.Product;
+import bca.Hapepedia.dto.ResponseData;
 import bca.Hapepedia.services.ProductService;
 
 @RestController
@@ -15,8 +17,23 @@ public class ProductApiController {
     private ProductService productService;
 
     @GetMapping
-    public Iterable<Product> findAll()
+    public ResponseEntity<ResponseData> findAll()
     {
-        return productService.findAll();
+        ResponseData response = new ResponseData();
+        try
+        {
+            response.setStatus(true);
+            response.getMessages().add("Products found");
+            response.setPayload(productService.findAll());
+
+            return ResponseEntity.ok(response);
+        }
+
+        catch(Exception ex)
+        {
+            response.setStatus(false);
+            response.getMessages().add("Could not load products: " + ex.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
     }
 }
