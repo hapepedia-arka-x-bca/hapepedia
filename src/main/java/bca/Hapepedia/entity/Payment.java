7873b6fa03b1e842742e4bc6803801a815f334e1
annisaaadindra <annisaaadindra@gmail.com>
2020-10-13 12:14:14 +0700

    payment repo+entity+service

---
+++ src/main/java/bca/Hapepedia/entity/Payment.java
@@ -0,0 +1,52 @@
+package bca.Hapepedia.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_payment")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    Order order;
+
+    @Column
+    private Timestamp paymentDate;
+
+    @Column
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
