fd06df44aa9ff0601f20d6b8bd6c87f14121cda3
annisaaadindra <annisaaadindra@gmail.com>
2020-10-15 00:42:11 +0700

    Update CartService.java

--- src/main/java/bca/Hapepedia/services/CartService.java
+++ src/main/java/bca/Hapepedia/services/CartService.java
@@ -16,7 +16,7 @@ public class CartService {
 	@Autowired
 	private CartRepo cartRepo;
 	
-	public Iterable<Cart> findAllByCustomer (Long customer) {
+	public Iterable<Cart> findAllByCustomer (Customer customer) {
 		return cartRepo.findAllByCustomer(customer);
 	}
 	
