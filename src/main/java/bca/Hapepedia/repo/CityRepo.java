647b76d8f02b17c6aad8ebc3d28bfba234a60c63
willyaditya <willyaditya03@gmail.com>
2020-10-14 12:17:40 +0700

    Shipping Address Module (controller and dto & entity atribute fixes)

--- src/main/java/bca/Hapepedia/repo/CityRepo.java
+++ src/main/java/bca/Hapepedia/repo/CityRepo.java
@@ -1,8 +1,15 @@
 package bca.Hapepedia.repo;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import bca.Hapepedia.entity.City;
+import bca.Hapepedia.entity.Province;
 
 public interface CityRepo extends CrudRepository<City, String>{
-
+	public Iterable<City> findAll();
+	
+	public Optional<City> findById(Long id);
+	
+	public Iterable<City> findAllByProvince(Province province);
 }
